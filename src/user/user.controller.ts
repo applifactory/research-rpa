77f50d2458fad8d7e57e79b5a5743fd3a75fdf6e
Mateusz Witaliński <matthew.w@314t.com>
2019-04-23 22:07:43 +0200

    Add authorized user profile endpoint

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,21 @@
-import { Controller, UseGuards } from '@nestjs/common';
+import { Controller, UseGuards, Get, Req, NotFoundException } from '@nestjs/common';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { UserService } from './user.service';
+import { JwtRequest } from 'src/auth/interfaces/jwt-request.interface';
 
 @Controller('user')
 @UseGuards(JwtAuthGuard)
-export class UserController {}
+export class UserController {
+
+  constructor(private readonly userService: UserService) {}
+
+  @Get()
+  async currentUser(@Req() request: JwtRequest) {
+    const user = await this.userService.findById(request.user._id.toString());
+    if ( !user ) {
+      throw new NotFoundException('User not found');
+    }
+    return user;
+  }
+
+}
